717b8ca573278aa407f0f7d8fad04e5c0b4897aa
Dmitriy Tikhomirov <uroborosq@ya.ru>
2025-06-15 00:03:51 +0300

    sdfsd

--- src/cpu_usage.nim
+++ src/cpu_usage.nim
@@ -1,10 +1,20 @@
-proc readUsage(): string = 
-  let content = readLines("/proc/stat", 1)
-  echo content
-  return content[0]
+import strutils
+import std/os
+import std/sequtils
+import std/strformat
 
+proc readUsage(): string = readLines("/proc/stat", 1)[0]
 
+func parseStatLine(line: string): seq[uint] = line.splitWhitespace[1..^1].map(parseUint)
+
+func calculate(stats: seq[uint]): tuple[work: uint, total: uint] =
+  let work = stats[0..<3].foldl(a+b)
+  return (work: work, total: stats[3..^1].foldl(a+b)+work)
 
 when isMainModule:
-  let c = readUsage()
-  echo c
+
+
+  let initial = readUsage().parseStatLine().calculate()
+  sleep(1000)
+  let new = readUsage().parseStatLine().calculate()
+  echo fmt"{float(new.work - initial.work) / float(new.total - initial.total) * 100:.1f}%"
--- src/ram.nim
+++ src/ram.nim
@@ -1,13 +1,20 @@
 import strutils
 import std/strformat
+import cligen
 
-func getBytes(line: string): uint = return parseUint(line.splitWhitespace()[1]) * 1024
 
-func toGigaBytes(bytes: uint): float = return float(bytes) / 1024 / 1024 / 1024
+func bytes(line: string): uint = line.splitWhitespace[1].parseUint * 1024
+
+func toGB(bytes: uint): float = float(bytes) / 1024 / 1024 / 1024
+
+proc cpu_usage(typ: string = "default"): void =
+    echo "Type: ", typ
 
 when isMainModule:
-  let lines = readFile("/proc/meminfo").splitLines()
-  let usedRam = lines[0].getBytes.toGigaBytes - lines[2].getBytes.toGigaBytes
-  let usedSwap = lines[14].getBytes.toGigaBytes - lines[15].getBytes.toGigaBytes
+  dispatch cpu_usage
+
+  let lines = readFile("/proc/meminfo").splitLines
+  let usedRam = lines[0].bytes().toGB() - lines[2].bytes().toGB()
+  let usedSwap = lines[14].bytes().toGB() - lines[15].bytes().toGB()
   echo fmt"{usedRam:.1f} GiB {usedSwap:.1f} GiB"
 
