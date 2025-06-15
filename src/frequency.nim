f75b671098915731fcd4cab5543dc0d4f36ebf22
Dmitriy Tikhomirov <uroborosq@ya.ru>
2025-06-16 02:05:05 +0300

    use faster approach

--- src/frequency.nim
+++ src/frequency.nim
@@ -1,13 +1,18 @@
+import std/strformat
+import std/cpuinfo
 import strutils
-import std/[asyncdispatch, asyncfile, sequtils, strformat, cpuinfo]
+import malebolgia
 
-proc getFreq(coreNumber: int): Future[uint] {.async.} = 
-  var f = openAsync(fmt"/sys/devices/system/cpu/cpu{coreNumber}/cpufreq/scaling_cur_freq", fmRead)
-  let freq = await f.readAll()
-  f.close()
-  return freq.strip().parseUint() div 1000
+proc getFrequency(coreNumber: int): uint =
+  return readFile(fmt"/sys/devices/system/cpu/cpu{coreNumber}/cpufreq/scaling_cur_freq").strip().parseUint() div 1000
 
 when isMainModule:
-  let freqs = waitFor toSeq(0..<countProcessors()).map(getFreq).all()
-  echo fmt"{freqs.max()}MHz"
+  var m = createMaster()
 
+  var freqs = newSeq[uint](countProcessors())
+
+  m.awaitAll:
+    for i in 0..<countProcessors():
+      m.spawn getFrequency(i) -> freqs[i] 
+
+  echo fmt"{freqs.max()}MHz"
